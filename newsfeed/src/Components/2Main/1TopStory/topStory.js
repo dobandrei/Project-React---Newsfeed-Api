86b9fa1fe6160edf579e189f15858ba9bd654ba4
dobandrei <dob_andrei@yahoo.com>
2020-04-30 12:41:37 +0300

    I put eventListener on text so it opens the news in the original location

--- newsfeed/src/Components/2Main/1TopStory/topStory.js
+++ newsfeed/src/Components/2Main/1TopStory/topStory.js
@@ -2,15 +2,14 @@ import React from 'react';
 import '../main.css';
 
 function TopStory({data}) {
-
+  const handleClick = () => window.open(data.url)
   return (
-    
     <div className="containerTopStory">
       <img src ={data.urlToImage} alt = {`image ${data.title}`}/>
         <title>{data.title}</title>
         <h2>Author: {data.author}</h2>
         <span>Published date: {data.publishedAt.slice(0,10)}</span> 
-        <p>{data.description}</p>
+        <p onClick = {handleClick}>{data.description}</p>
         
     </div>
   );
--- newsfeed/src/Components/2Main/2News/news.js
+++ newsfeed/src/Components/2Main/2News/news.js
@@ -2,13 +2,15 @@ import React from 'react';
 import '../main.css';
 
 function News({data}) {
+  const handleClick = () => window.open(data.url)
+  
   return (
     <div className="container">
         <img src ={data.urlToImage} alt = {`image ${data.title}`}/>
         <title>{data.title}</title>
-        <h2>Author: {data.author}</h2>
+        <h2>Author: {data.author != null ? data.author : 'No Author' }</h2>
         <span>Published date: {data.publishedAt.slice(0,10)}</span>  
-        <p>{data.description}</p>
+        <p onClick = {handleClick}>{data.description}</p>
             
     </div>
   );
